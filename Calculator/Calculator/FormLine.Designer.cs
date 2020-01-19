4ae1917bacd581f6a96a04f3772c17c0945f6bc5
yuri <y.yuferev@mail.ru>
2020-01-19 18:11:27 +0300

    Фиговина вместо линейной

--- Calculator/Calculator/FormLine.Designer.cs
+++ Calculator/Calculator/FormLine.Designer.cs
@@ -10,10 +10,11 @@ namespace Calculator
 {
 	partial class FormLine
 	{
-		/// <summary>
+/// <summary>
 		/// Designer variable used to keep track of non-visual components.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -36,71 +37,115 @@ namespace Calculator
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.button1 = new System.Windows.Forms.Button();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.button3 = new System.Windows.Forms.Button();
 			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.trackBar1 = new System.Windows.Forms.TrackBar();
+			this.trackBar2 = new System.Windows.Forms.TrackBar();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
 			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(12, 430);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(17, 20);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "X";
+			// 
+			// textBox1
+			// 
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox1.Location = new System.Drawing.Point(35, 430);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(33, 20);
+			this.textBox1.TabIndex = 4;
 			// 
 			// button1
 			// 
 			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
 			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.button1.Location = new System.Drawing.Point(118, 427);
+			this.button1.Location = new System.Drawing.Point(74, 430);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(101, 23);
-			this.button1.TabIndex = 0;
+			this.button1.Size = new System.Drawing.Size(108, 23);
+			this.button1.TabIndex = 6;
 			this.button1.Text = "построить";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.Button1Click);
 			// 
-			// textBox1
+			// button2
 			// 
-			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.textBox1.Location = new System.Drawing.Point(34, 430);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(78, 20);
-			this.textBox1.TabIndex = 1;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button2.Location = new System.Drawing.Point(364, 433);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(108, 23);
+			this.button2.TabIndex = 7;
+			this.button2.Text = "Выход";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
 			// 
-			// button3
+			// trackBar1
 			// 
-			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.button3.Location = new System.Drawing.Point(364, 427);
-			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(108, 23);
-			this.button3.TabIndex = 8;
-			this.button3.Text = "Выход";
-			this.button3.UseVisualStyleBackColor = true;
-			this.button3.Click += new System.EventHandler(this.Button3Click);
+			this.trackBar1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.trackBar1.LargeChange = 8;
+			this.trackBar1.Location = new System.Drawing.Point(3, 0);
+			this.trackBar1.Maximum = 32;
+			this.trackBar1.Name = "trackBar1";
+			this.trackBar1.Size = new System.Drawing.Size(226, 42);
+			this.trackBar1.SmallChange = 2;
+			this.trackBar1.TabIndex = 7;
+			this.trackBar1.ValueChanged += new System.EventHandler(this.TrackBar1ValueChanged);
 			// 
-			// label1
+			// trackBar2
 			// 
-			this.label1.Location = new System.Drawing.Point(2, 432);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(26, 11);
-			this.label1.TabIndex = 9;
-			this.label1.Text = "Х";
+			this.trackBar2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.trackBar2.Location = new System.Drawing.Point(235, 0);
+			this.trackBar2.Maximum = 90;
+			this.trackBar2.Name = "trackBar2";
+			this.trackBar2.Size = new System.Drawing.Size(231, 42);
+			this.trackBar2.SmallChange = 5;
+			this.trackBar2.TabIndex = 8;
+			this.trackBar2.ValueChanged += new System.EventHandler(this.TrackBar2ValueChanged);
 			// 
-			// Form5
+			// FormLine
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(484, 462);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.button3);
+			this.BackColor = System.Drawing.SystemColors.Control;
+			this.ClientSize = new System.Drawing.Size(492, 473);
+			this.Controls.Add(this.trackBar2);
+			this.Controls.Add(this.trackBar1);
 			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
 			this.Controls.Add(this.button1);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-			this.Name = "Form5";
+			this.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.MinimumSize = new System.Drawing.Size(500, 500);
+			this.Name = "FormLine";
 			this.ShowIcon = false;
-			this.Text = "Линейная функция";
+			this.Text = "Sin";
+			this.Activated += new System.EventHandler(this.FormLineShown);
+			this.Load += new System.EventHandler(this.FormLineLoad);
+			this.Shown += new System.EventHandler(this.FormLineShown);
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormLinePaint);
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 		}
+		private System.Windows.Forms.TrackBar trackBar2;
+		private System.Windows.Forms.TrackBar trackBar1;
+		private System.Windows.Forms.Button button2;
 		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Button button3;
-		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.TextBox textBox1;
+		
+		void FormLineLoad(object sender, System.EventArgs e)
+		{
+			
+		}
+		
 	}
 }
--- Calculator/Calculator/FormLine.cs
+++ Calculator/Calculator/FormLine.cs
@@ -20,6 +20,19 @@ namespace Calculator
 		public FormLine()
 		{
 			InitializeComponent();
+			this.Layout += new System.Windows.Forms.LayoutEventHandler(this.linLayout);
+			Graphics();
+		}
+		
+		void PictureBox1Paint(object sender, PaintEventArgs e)
+		{
+			
+		}
+		private float Y(float y)
+		{
+			float wy = (float)this.Height;
+				
+				return  - y;
 		}
 		void Graphics()
 		{
@@ -27,27 +40,103 @@ namespace Calculator
 			Pen pen = new Pen(Color.Blue);
 			Pen pe = new Pen(Color.Red);
 			g.Clear(Color.White);
-			g.TranslateTransform(250.0F, 250.0F);
+			int wy = this.Height;
+			int wx = this.Width;
+			g.TranslateTransform((float)wx/2, (float)wy/2);
 			
-			int x = int.Parse(textBox1.Text);
-			int y = 2*x;
-			g.DrawLine(pen, x, y, -x, -y);
-				
-				
-			g.DrawLine(pe, 0, -1000, 0, 1000);
-			g.DrawLine(pe, -1000, 0, 1000, 0);
+			int ky = (int)(0.75 * wy/2); //200;
+			int kx = (int)(0.75 * wx/2);
+			float alfa = 0.0F;			
+			float ox = (float) Math.Sin(alfa);
+			float oy = ky * (float)Math.Cos(alfa); // Предыдущие значения
+			float cx = ox, cy; // Текущие значения
+			int k;
+			int fi;
+			k = this.trackBar1.Value;
+			fi = this.trackBar2.Value;
+			for (alfa = 0; alfa < 720; alfa++)
+			{
+				 
+				cx = (float)Math.Sin(alfa*Math.PI/180)*kx;
+				cy = (float)Math.Cos(k*(alfa + fi) *Math.PI/180)*ky;
+				if ( cx > wx/2) break;
+			    g.DrawLine(pen, ox, Y(oy), cx, Y(cy));
+				ox = cx;
+				oy = cy;				
+			}
+			
+			g.DrawLine(pe, 0, Y(-wy/2), 0, Y(wy/2));
+			g.DrawLine(pe, 0, Y(wy/2), 20, Y(wy/2-20));
+			g.DrawLine(pe, 0, Y(wy/2), -20, Y(wy/2-20));
+			g.DrawLine(pe, -wx/2, 0, wx/2, 0);
+			g.DrawLine(pe, wx/2-8, 0, wx/2-30, -20);
+			g.DrawLine(pe, wx/2-8, 0, wx/2-30, 20);
+			
+			for (int i = 45; i < wx/2-10; i += 45) {
+				g.DrawLine(pe, i, 5, i, -5);
+				g.DrawLine(pe, -i, 5, -i, -5);
+				int t = (int)(i / kx);
+				int tt = -t;
+				g.DrawString(t.ToString(), Font, Brushes.Black, i-5, 16);
+				g.DrawString(tt.ToString(), Font, Brushes.Black, -i-5, 16);
+			}
+			for (int i = 0; i < wy/2-20; i += ky) {
+				g.DrawLine(pe, -5, i, 5, i);
+				g.DrawLine(pe, -5, -i, 5, -i);
+				int t = (int)(i / ky);
+				int tt = -t;
+				g.DrawString(t.ToString(), Font, Brushes.Black, -20, -i - 10);
+				g.DrawString(tt.ToString(), Font, Brushes.Black, -20, i - 10);
+			}
+			
+			string name = "Лёшик балбес ";
+			int n = name.Length;
+			float angleStep = 360 / n;
+			Char[] c;
+			c = name.ToCharArray(0,name.Length);
+			Font f  = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+
+			for (int i=0; i<n; i++)
+			{
+				string s = string.Join("",c[i]);
+				g.RotateTransform(angleStep);
+				g.DrawString(s, f, Brushes.Black, 0, -60);
+			}
+			g.ResetTransform();
 		}
-		void Button1Click(object sender, System.EventArgs e)
+		void Button1Click(object sender, EventArgs e)
 		{
 			Graphics();
 		}
 		void Button2Click(object sender, System.EventArgs e)
 		{
-		
-		}
-		void Button3Click(object sender, System.EventArgs e)
-		{
 			this.Close();
+			
+		}
+		private void linLayout(object sender, System.Windows.Forms.LayoutEventArgs e)
+		{
+//		    int wy = this.Height;
+//			int wx = this.Width;
+			Button1Click(sender, e);
+		}
+	    void FormLineShown(object sender, System.EventArgs e)
+		{
+			Graphics();
+		}
+	    void FormLinePaint(object sender, System.Windows.Forms.PaintEventArgs e)
+		{
+			Graphics();
+		}
+	
+		
+		void TrackBar1ValueChanged(object sender, EventArgs e)
+		{
+			Graphics();
+		}
+		
+		void TrackBar2ValueChanged(object sender, EventArgs e)
+		{
+			Graphics();
 		}
 	}
 }
